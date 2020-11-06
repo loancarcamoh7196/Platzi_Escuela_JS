323ee7f0afbf962ac8caf795183f0e0da6691565
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2020-11-06 10:51:29 -0300

    video 9 & 10 - Instalr y usar Babel ~ Webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Archivo configuracion de proyecto
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Mis componentes
+import HelloWorld from './components/HelloWorld'
+
+
+ReactDOM.render(<HelloWorld />, document.getElementById(app));
