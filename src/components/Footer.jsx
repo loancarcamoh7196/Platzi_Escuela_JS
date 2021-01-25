90a404873998d5963a9761df933748dbb2f39661
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-01-25 13:11:33 -0300

    Limpiar y poner regla codigo ~ Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import '../assets/styles/components/Footer.scss';
 /**
  * Constantes para texto elementos html
  */
-const footerEndTermsURL = "#";
-const footerEndTermsText = "Términos de uso";
-const footerDeclarationPrivacityURL = "#";
-const footerDeclarationPrivacityText = "Declaración de privacidad";
-const footerHelpCenterURL = "#";
-const footerHelpCenterText = "Centro de ayuda";
+const footerEndTermsURL = '#';
+const footerEndTermsText = 'Términos de uso';
+const footerDeclarationPrivacityURL = '#';
+const footerDeclarationPrivacityText = 'Declaración de privacidad';
+const footerHelpCenterURL = '#';
+const footerHelpCenterText = 'Centro de ayuda';
 
 const Footer = () => (
-    <footer className="footer">
-        <a href={ footerEndTermsURL }> { footerEndTermsText } </a>
-        <a href={ footerDeclarationPrivacityURL }> { footerDeclarationPrivacityText } </a>
-        <a href={ footerDeclarationPrivacityURL }> { footerHelpCenterText } </a>
-    </footer>
+  <footer className='footer'>
+    <a href={footerEndTermsURL}> {footerEndTermsText} </a>
+    <a href={footerDeclarationPrivacityURL}> {footerDeclarationPrivacityText} </a>
+    <a href={footerHelpCenterURL}> {footerHelpCenterText} </a>
+  </footer>
 );
 
 export default Footer;
