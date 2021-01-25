9e516e45c6d52ce60263e0894dbd634a60630be2
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-01-25 13:06:44 -0300

    Limpiar y poner regla codigo ~ Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,34 +4,32 @@ import '../assets/styles/components/Header.scss';
 /**
  * Constantes para texto elementos html
  */
-import  headerLogoLandingPageURL from '../assets/static/logo-platzi-video-BW2.png';
-const headerLogoLandingPageAlt = "Video-Logo";
-import headerUserLandingPageURL from "../assets/static/user-icon.png";
-const headerMenuTitle = "Perfil";
-const headerUserLandingPageAlt = "Usuario ;)";
-const headerMenuAccountURL = "#";
-const headerMenuAccountText = "Cuenta";
-const headerMenuSessionURL = "#";
-const headerMenuSessionText = "Cerrar Sesión";
+import headerLogoLandingPageURL from '../assets/static/logo-platzi-video-BW2.png';
+import headerUserLandingPageURL from '../assets/static/user-icon.png';
 
+const headerLogoLandingPageAlt = 'Video-Logo';
+const headerMenuTitle = 'Perfil';
+const headerUserLandingPageAlt = 'Usuario ;)';
+const headerMenuAccountURL = '#';
+const headerMenuAccountText = 'Cuenta';
+const headerMenuSessionURL = '#';
+const headerMenuSessionText = 'Cerrar Sesión';
 
 const Header = () => (
-    <header className="header">
-        <img className="header__img" src={ headerLogoLandingPageURL } alt={ headerLogoLandingPageAlt } />
-        <div className="header__menu">
-            <div className="header__menu--profile">
-            <img src={ headerUserLandingPageURL } alt={ headerUserLandingPageAlt } />
-            <p>{ headerMenuTitle }</p>
-            </div>
+  <header className='header'>
+    <img className='header__img' src={headerLogoLandingPageURL} alt={headerLogoLandingPageAlt} />
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={headerUserLandingPageURL} alt={headerUserLandingPageAlt} />
+        <p>{ headerMenuTitle }</p>
+      </div>
 
-            <ul>
-            <li><a href={ headerMenuAccountURL }>{ headerMenuAccountText }</a></li>
-            <li><a href={ headerMenuSessionURL }>{ headerMenuSessionText }</a></li>
-            
-            </ul>
-        </div>
-    </header>
+      <ul>
+        <li><a href={headerMenuAccountURL}>{headerMenuAccountText}</a></li>
+        <li><a href={headerMenuSessionURL}>{headerMenuSessionText}</a></li>
+      </ul>
+    </div>
+  </header>
 );
-
 
 export default Header;
