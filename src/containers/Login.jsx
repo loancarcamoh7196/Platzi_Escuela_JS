fa1883d0ed976a46ef7cd6bfe84a6e6b58bae521
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-01-26 14:20:08 -0300

    video 4 - Configurar entorno de desarrollo para uso de rutas. Ademas de agregar contenedor-componente Login a la APP

---
+++ src/containers/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../assets/styles/components/Login.scss';
+import googleIcon from '../assets/static/google-icon.png';
+import twitterIcon from '../assets/static/twitter-icon.png';
+
+const Login = () => (
+  <section className='login'>
+    <section className='login__container'>
+      <h2 className='login-title'>Inicia Sesión</h2>
+      <form action='' className='login__container--form'>
+
+        <input type='text' className='input' placeholder='Correo' />
+        <input type='password' className='input' placeholder='Contraseña' />
+        <button className='button'> Iniciar Sesión </button>
+        <div className='login__container--remember--me'>
+          <label>
+            <input type='checkbox' name='' id='cbox1' value='checkbox' />
+            Recuerdame
+          </label>
+          <a href='#'> Olvide mi contraseña </a>
+        </div>
+      </form>
+      <section className='login__container--social-media'>
+        <div>
+          <img src={googleIcon} alt='Google' />
+          Inicia sesión con Google
+        </div>
+        <div>
+          <img src={twitterIcon} alt='Twitter' />
+          Inicia sesión con Twitter
+        </div>
+      </section>
+      <p className='login__container--register'>
+        No tienes ninguna cuenta? <a href='#'> Regístrate</a>
+      </p>
+    </section>
+  </section>
+);
+
+export default Login;
