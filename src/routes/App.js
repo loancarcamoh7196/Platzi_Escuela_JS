4ad8b3b8aaa60311bc74767da5853ef38b3758ec
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-01-26 17:36:29 -0300

    video 4 - Configurar entorno de desarrollo para uso de rutas. Ademas de agregar contenedor-componente Login a la APP

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 /***
  * Esta es la importancion para el uso de paquete React Route
- */ 
+ */
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
