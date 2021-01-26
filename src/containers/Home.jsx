06aad50c05504160bd2a5f583d4385bd03357614
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-01-26 13:11:36 -0300

    video 1 a 3 ~ Instalacion e implementacion de paquete React Routes

---
+++ src/containers/Home.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+//Hoja de estilo contenedor
+import '../assets/styles/App.scss';
+
+/**
+ * Mis componentes
+ */
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+/**
+ * Mis Hooks
+ */
+import useInitialState from '../hooks/useInitialState';
+
+/**
+ * Mis API
+ */
+const API = 'http://localhost:3000/initialState'; // Mi servicio API ~ debe estar corriendo
+
+const Home = () => {
+  const [videos, categories] = useInitialState(API); // Declaracion de uso de API
+
+  return (
+    <div className='Home'>
+      <Header />
+      <Search />
+      {
+        categories.map((category) => (
+          videos[category].length > 0 && (
+            <Categories title={category}>
+              <Carousel>
+                {videos[category].map((item) => <CarouselItem key={item.id} { ...item } />) }
+              </Carousel>
+            </Categories>
+
+          )
+        ))
+      }
+      <Footer />
+    </div>
+  );
+};
+export default Home;
