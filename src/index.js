5447391f21b0a08b610dcb830076cbd8fa9f44b0
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-02-03 18:45:51 -0300

    video 11 - Instalacion de Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 /**
  * Archivo configuracion de proyecto
+ * 
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStpre } from 'redux';
 
 /***
- * El ecompoenente principal ha sido reemplaado por
+ * El componente principal ha sido reemplaado por
  */
 import App from './routes/App'; //<-- Contenedor Principal
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
