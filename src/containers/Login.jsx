7647bc08a9086fa32f4bf93a9e5bce97ed031fe8
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-03-17 12:52:09 -0300

    video 16 ~ Crear formulario Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,44 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2 className='login-title'>Inicia Sesión</h2>
-      <form action='' className='login__container--form'>
+const Login = () => {
+  const [ form, setValues ] = useState({
+    email: '',
 
-        <input type='text' className='input' placeholder='Correo' />
-        <input type='password' className='input' placeholder='Contraseña' />
-        <button className='button'> Iniciar Sesión </button>
-        <div className='login__container--remember--me'>
-          <label>
-            <input type='checkbox' name='' id='cbox1' value='checkbox' />
-            Recuerdame
-          </label>
-          <a href='#'> Olvide mi contraseña </a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} alt='Google' />
-          Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt='Twitter' />
-          Inicia sesión con Twitter
-        </div>
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form)
+  };
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2 className='login-title'>Inicia Sesión</h2>
+        <form
+          className='login__container--form'
+          onSubmit={handleSubmit}
+        >
+
+          <input
+            name='email'
+            type='text'
+            className='input'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            type='password'
+            className='input'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button' type='submit'> Iniciar Sesión </button>
+
+
+          <div className='login__container--remember--me'>
+            <label>
+              <input type='checkbox' name='' id='cbox1' value='checkbox' />
+              Recuerdame
+            </label>
+            <a href='#'> Olvide mi contraseña </a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} alt='Google' />
+            Inicia sesión con Google
+          </div>
+          <div>
+            <img src={twitterIcon} alt='Twitter' />
+            Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          No tienes ninguna cuenta?
+          <Link to='/register'>
+            Regístrate
+          </Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        No tienes ninguna cuenta?  
-        <Link to='/register'>
-          Regístrate
-        </Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
 export default Login;
