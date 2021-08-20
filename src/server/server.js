24ae958a3f1d3b37bc0d804967b7fa3dd8c0dac9
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-08-20 12:46:46 -0400

    [feature/server-side-render-2020] Implementando nodemon y dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();// Define y buscr variable de entorno
+const {ENV, PORT} = process.env;
 
 const app = express();
 
+if(ENV === 'development') {
+  console.log('DEV MODE');
+} else if (ENV === 'prouction') {
+  console.log('PRODUCTION MODE');
+}
+
 app.get('*', (req, res) => {
-  res.send({ hello: 'express' });
+  res.send({ hello: 'Hola, ten un buen día' });
 });
 
-app.listen(3000, (err) => {
-  (err) ? console.log('ERROR: ', err) : console.log('Server corriendo en puerto 3000 ...');
+app.listen(PORT, (err) => {
+  (err) ? console.log('ERROR: ', err) : console.log(`Server corriendo en puerto ${PORT} ...`);
 });
