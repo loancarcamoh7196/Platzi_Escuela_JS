1b014bb2dd0e9d912425d1e2a9c9c1c66a9b6d64
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-08-20 16:53:55 -0400

    [feature/server-side-render-2020 ~ 2021-08-20] Arreglar pagina de Error 404

--- src/frontend/containers/NotFound.jsx
+++ src/frontend/containers/NotFound.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/NotFound.scss';
-import notFoundImage from '../assets/static/404.png';
 
 const NotFound = () => (
-  <section className='not-found'>
-    <div className='box'>
-      <img src={notFoundImage} alt='' className='text animated rubberBand' />
+  <section id='notfound'>
+    <div className='notfound'>
+      <div className='notfound-404'>
+        <h1>404</h1>
+        <h2>Te perdiste?</h2>
+      </div>
+      <Link to='/'>
+        Vuelve al Inicio
+      </Link>
     </div>
-    <h5> Auch, algo se rompio</h5>
-    <h4>Te perdiste?</h4>
-    <Link to='/'>
-      Volver al Inicio
-    </Link>
   </section>
 
 );
