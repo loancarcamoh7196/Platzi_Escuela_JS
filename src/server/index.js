8402723b5d1ead864065067f03149396655216a4
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-08-23 12:06:42 -0400

    [feature/server-side-render-2020 ~ 2021-08-23] Implementacion de funcion principal de renderizado

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,7 @@
+require('ignore-styles');//Obliga al servidor a ignorar los llamdos de estilos css
+
 require('@babel/register')({
   presets: ['@babel/preset-env', '@babel/preset-react'],
 });
 
-require('./server');
+require('./server');// Archivo de configuracion de Servicio HTTP
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,26 @@
+/**
+ * Archivo Configuración de Servicio HTTP ~ Servidor
+ * Lado Backend
+ */
 import express from 'express';
-import webpack from 'webpack';
+import webpack from 'webpack';// Modele Bundler
+import React from 'react';
+import { renderToString } from 'react-dom/server'; // Renderiza las respuesta dell server a String
+import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';// Recibe Array de ruta de lado Servidor
+import { createStore } from 'redux';
+// import { StaticRouter } from 'react-router';
+import { StaticRouter } from 'react-router-dom';
+import reducer from '../frontend/reducers';
+import serverRoutes from '../frontend/routes/serverRoutes';// Array de Rutas
+import initialState from '../frontend/initialState'; // Archivo mocks con Data
 
-const { config } = require('../../config');
+
+const { config } = require('../../config');// Archivo de config de Variables de Entorno
 
 const app = express();
 
-if(config.env === 'development') {
+if (config.env === 'development') {
   console.log('DEV MODE');
   const webpackConfig = require('../../webpack.config');
   const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -21,19 +36,48 @@ if(config.env === 'development') {
   console.log('PRODUCTION MODE');
 }
 
+/**
+ * Inserta en codigo html renderizado dentro del Contenedor de la página
+ * @param {String} html Contenido renderizado
+ * @returns String Texto completo a desplegar de la página
+ */
+const setResponse = (html) =>{
+  return(`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" href="assets/app.css" type="text/css">
+      <title>Platzi Video</title>
+    </head>
+    <body>
+      <div id="app">${html}</div>
+      <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+  </html>
+  `)
+};
+
+/**
+ * Renderizada página solicitada
+ * @param {*} req Petición http
+ * @param {*} res Respuesta renderizada
+ */
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider>
+      <StaticRouter location={store} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+
+  res.send(setResponse(html)); // contenido de la pagina ya renderezado carga
+}
+
 app.get('*', (req, res) => {
   res.send(`
-  <!DOCTYPE html>
-    <html>
-      <head>
-        <link rel="stylesheet" href="assets/app.css" type="text/css">
-        <title>Platzi Video</title>
-      </head>
-      <body>
-        <div id="app"></div>
-        <script src="assets/app.js" type="text/javascript"></script>
-      </body>
-    </html>
+ 
   `);
 });
 
